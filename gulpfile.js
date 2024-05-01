a2f6c6265e1a953fd6bcfd952802a213f3c364e7
Gabrielbg211 <Gbustosgarcia01@gmail.com>
2024-05-01 00:48:35 -0300

    Done

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+// Tarea para compilar el archivo skeleton.scss
+gulp.task('compile-sass', function () {
+    return gulp.src('sass/base/skeleton.scss') // Ruta al archivo skeleton.scss
+        .pipe(sass().on('error', sass.logError)) // Compilar Sass
+        .pipe(gulp.dest('css')); // Directorio de destino para el CSS compilado
+});
+
+// Tarea de observación para compilar Sass automáticamente cuando haya cambios
+gulp.task('watch', function () {
+    gulp.watch('sass/**/*.scss', gulp.series('compile-sass'));
+});
+
+// Tarea predeterminada para ejecutar todas las tareas de una vez
+gulp.task('default', gulp.series('compile-sass', 'watch'));
