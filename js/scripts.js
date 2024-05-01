97e6f5f29d6083dde92c15420a995f276ae68399
Gabrielbg211 <Gbustosgarcia01@gmail.com>
2024-05-01 18:38:43 -0300

    dONE

--- js/scripts.js
+++ js/scripts.js
@@ -43,10 +43,10 @@
         });
  });
     
-
+// modal 1
      // Obtén una referencia al botón "Ver más" y al modal
-    var openModalButton = document.getElementById('openModalButton');
-    var modal = document.getElementById('myModal');
+    var openModalButton = document.getElementById('openModalButton1');
+    var modal = document.getElementById('myModal1');
 
     // Agrega un event listener al botón para abrir el modal cuando se hace clic
     openModalButton.addEventListener('click', function() {
@@ -57,3 +57,79 @@
     modal.querySelector('.modal-close').addEventListener('click', function() {
         modal.classList.remove('is-active'); // Quita la clase 'is-active' para ocultar el modal
     });
+    
+// modal 2
+     // Obtén una referencia al botón "Ver más" y al modal
+    var openModalButton2 = document.getElementById('openModalButton2');
+    var modal2 = document.getElementById('myModal2');
+
+    // Agrega un event listener al botón para abrir el modal cuando se hace clic
+    openModalButton2.addEventListener('click', function() {
+        modal2.classList.add('is-active'); // Añade la clase 'is-active' para mostrar el modal
+    });
+
+    // Agrega un event listener al botón de cierre del modal para cerrarlo cuando se hace clic
+    modal2.querySelector('.modal-close').addEventListener('click', function() {
+        modal2.classList.remove('is-active'); // Quita la clase 'is-active' para ocultar el modal
+    });
+    
+// modal 3
+     // Obtén una referencia al botón "Ver más" y al modal
+    var openModalButton3 = document.getElementById('openModalButton3');
+    var modal3 = document.getElementById('myModal3');
+
+    // Agrega un event listener al botón para abrir el modal cuando se hace clic
+    openModalButton3.addEventListener('click', function() {
+        modal3.classList.add('is-active'); // Añade la clase 'is-active' para mostrar el modal
+    });
+
+    // Agrega un event listener al botón de cierre del modal para cerrarlo cuando se hace clic
+    modal3.querySelector('.modal-close').addEventListener('click', function() {
+        modal3.classList.remove('is-active'); // Quita la clase 'is-active' para ocultar el modal
+    });
+    
+// modal 4
+     // Obtén una referencia al botón "Ver más" y al modal
+    var openModalButton4 = document.getElementById('openModalButton4');
+    var modal4 = document.getElementById('myModal4');
+
+    // Agrega un event listener al botón para abrir el modal cuando se hace clic
+    openModalButton4.addEventListener('click', function() {
+        modal4.classList.add('is-active'); // Añade la clase 'is-active' para mostrar el modal
+    });
+
+    // Agrega un event listener al botón de cierre del modal para cerrarlo cuando se hace clic
+    modal4.querySelector('.modal-close').addEventListener('click', function() {
+        modal4.classList.remove('is-active'); // Quita la clase 'is-active' para ocultar el modal
+    });
+    
+// modal 5
+     // Obtén una referencia al botón "Ver más" y al modal
+    var openModalButton5 = document.getElementById('openModalButton5');
+    var modal5 = document.getElementById('myModal5');
+
+    // Agrega un event listener al botón para abrir el modal cuando se hace clic
+    openModalButton5.addEventListener('click', function() {
+        modal5.classList.add('is-active'); // Añade la clase 'is-active' para mostrar el modal
+    });
+
+    // Agrega un event listener al botón de cierre del modal para cerrarlo cuando se hace clic
+    modal5.querySelector('.modal-close').addEventListener('click', function() {
+        modal5.classList.remove('is-active'); // Quita la clase 'is-active' para ocultar el modal
+    });
+    
+// modal 6
+     // Obtén una referencia al botón "Ver más" y al modal
+    var openModalButton6 = document.getElementById('openModalButton6');
+    var modal6 = document.getElementById('myModal6');
+
+    // Agrega un event listener al botón para abrir el modal cuando se hace clic
+    openModalButton6.addEventListener('click', function() {
+        modal6.classList.add('is-active'); // Añade la clase 'is-active' para mostrar el modal
+    });
+
+    // Agrega un event listener al botón de cierre del modal para cerrarlo cuando se hace clic
+    modal6.querySelector('.modal-close').addEventListener('click', function() {
+        modal6.classList.remove('is-active'); // Quita la clase 'is-active' para ocultar el modal
+    });
+    
