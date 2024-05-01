52e295777328f1b0c9cae49f56499a4048687fd2
Gabrielbg211 <Gbustosgarcia01@gmail.com>
2024-05-01 16:56:33 -0300

    Done

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,41 @@
   });
 });
 
+ $(document).ready(function () {
+        var swiper = new Swiper('.swiper-container', {
+            loop: true, // Hace que el carousel sea infinito
+            effect: 'coverflow',
+            grabCursor: true,
+            centeredSlides: true,
+            slidesPerView: 'auto',
+            coverflowEffect: {
+                rotate: 50,
+                stretch: 0,
+                depth: 100,
+                modifier: 1,
+                slideShadows: true,
+            },
+            pagination: {
+                el: '.swiper-pagination',
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+ });
+    
+
+     // Obtén una referencia al botón "Ver más" y al modal
+    var openModalButton = document.getElementById('openModalButton');
+    var modal = document.getElementById('myModal');
+
+    // Agrega un event listener al botón para abrir el modal cuando se hace clic
+    openModalButton.addEventListener('click', function() {
+        modal.classList.add('is-active'); // Añade la clase 'is-active' para mostrar el modal
+    });
+
+    // Agrega un event listener al botón de cierre del modal para cerrarlo cuando se hace clic
+    modal.querySelector('.modal-close').addEventListener('click', function() {
+        modal.classList.remove('is-active'); // Quita la clase 'is-active' para ocultar el modal
+    });
