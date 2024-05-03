283014716f889a2bf3660a3395b44ac8cd2f313f
Gabriel Bustos <Gbustosgarcia01@gmail.com>
2024-05-03 03:08:14 -0300

    Done

--- js/scripts.js
+++ js/scripts.js
@@ -275,3 +275,43 @@ function formulario() {
         behavior: 'smooth'
     });
 }
+
+var nameField = document.forms["contact"]["name"];
+var emailField = document.forms["contact"]["email"];
+var messageField = document.forms["contact"]["message"];
+var ccarea = document.forms["contact"]["CountryCode[]"];
+var number = document.forms["contact"]["Phone"];
+var submitButton = document.getElementById("submitButton");
+
+function validateForm() {
+    if (nameField.value === "" || emailField.value === "" || messageField.value === "" || ccareaField.value === "" || numberField.value === "") {
+        alert("All fields must be filled out.");
+        return false;
+    }
+
+    // Validación adicional, por ejemplo, el formato de correo electrónico.
+    // Puedes agregar más validaciones aquí.
+
+    // Redirigir a index.html después de 3 segundos
+    setTimeout(function() {
+        window.location.href = "../index.html";
+    }, 3000);
+
+    return true;
+}
+
+// Habilitar o deshabilitar el botón de envío en tiempo real
+nameField.addEventListener("input", enableSubmitButton);
+emailField.addEventListener("input", enableSubmitButton);
+messageField.addEventListener("input", enableSubmitButton);
+ccarea.addEventListener("input", enableSubmitButton);
+number.addEventListener("input", enableSubmitButton);
+
+function enableSubmitButton() {
+    if (nameField.value !== "" && emailField.value !== "" && messageField.value !== "" && ccareaField.value !== "" && numberField.value !== "") {
+        submitButton.disabled = false;
+    } else {
+        submitButton.disabled = true;
+    }
+}
+
